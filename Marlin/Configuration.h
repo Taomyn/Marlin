75191c54b1c22bf4ab3db8ed75770ebfd04c58cf
Taomyn <ferd352+github@gmail.com>
2022-01-29 11:22:47 +0100

    Update Configuration.h
    Recalibrated Z-axis

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -932,7 +932,7 @@
  * Override with M92
  *                                      X, Y, Z [, I [, J [, K]]], E0 [, E1[, E2...]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 79.37, 79.72, 404.24, 143 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 79.37, 79.72, 402, 143 }
 
 /**
  * Default Max Feed Rate (mm/s)
