c54bd09cb73b52206913850584d70f2ec1b68382
Taomyn <ferd352+github@gmail.com>
2025-01-08 14:18:08 +0100

    Update Configuration.h
    Updated axis steps per unit

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1288,7 +1288,7 @@
  * Override with M92 (when enabled below)
  *                                      X, Y, Z [, I [, J [, K...]]], E0 [, E1[, E2...]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 79.37, 79.72, 402, 143 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80.25, 80.36, 407.50, 143.00 }
 
 /**
  * Enable support for M92. Disable to save at least ~530 bytes of flash.
